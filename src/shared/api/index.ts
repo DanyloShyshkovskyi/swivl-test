3f653e768a90fe5dd3d1af9b4fe3ed2b01c6fcd0
Danylo Shyshkovskyi <28-dsh@users.noreply.gitlab.localdomain>
2024-06-25 23:10:35 +0200

    Fix authorization

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -4,10 +4,8 @@ import { API_URL, GH_TOKEN } from 'shared/config'
 
 export const apiInstance = axios.create({
   baseURL: API_URL,
-
-  //add content type to header
   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + GH_TOKEN,
+    Authorization: GH_TOKEN ? 'Bearer ' + GH_TOKEN : undefined,
   },
 })
